6190c38bf106c4bd0d2e1e7acf20bb92cc250a96
himanshuhere <himanshusoni3105@outlook.com>
2020-08-09 14:27:23 +0530

    passport Facebook OAuth 2

--- authenticate.js
+++ authenticate.js
@@ -6,7 +6,7 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
 const FacebookTokenStrategy = require('passport-facebook-token');
 
-const keys = require('./config');
+const config = require('./config');
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -15,12 +15,12 @@ passport.deserializeUser(User.deserializeUser());
 
 exports.getToken = function(user) {
     console.log(user);
-    return jwt.sign(user, keys.secretKey, { expiresIn: 3600 });
+    return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
 };
 
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secretKey;
+opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(new JwtStrategy(opts,
     (jwt_payload, done) => {
@@ -51,3 +51,27 @@ exports.verifyAdmin = function(req, res, next) {
         }, (err) => next(err))
         .catch((err) => next(err))
 }
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        } else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}));
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '309884896894059',
+        clientSecret: '6ef4760ff347905888dc9147f728fcb1'
+    }
 }
